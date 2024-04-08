d92bb02b1e0b3e7a780ff47feaac63739def1fb3
rajvpatil5 <48220124+rajvpatil5@users.noreply.github.com>
2024-04-08 23:27:39 +0530

    change pdf file, change title and icon

--- src/components/CustomPdfViewer.jsx
+++ src/components/CustomPdfViewer.jsx
@@ -2,6 +2,8 @@ import { pdfjs } from "react-pdf";
 import { useState } from "react";
 import { Document, Page } from "react-pdf";
 import pdfFile from "../assets/pdf/certificate_of_publication-merged.pdf";
+import researchPaper from "../assets/pdf/research_paper.pdf";
+import certificate from "../assets/pdf/certificate_of_publication.pdf";
 import { Worker } from "@react-pdf-viewer/core";
 import { Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
@@ -9,23 +11,14 @@ import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 const CustomPdfViewer = () => {
-    // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-    // const pdfFile = "../assets/pdf/certificate_of_publication-merged.pdf";
-    // const [numPages, setNumPages] = useState(null);
-    // const [pageNumber, setPageNumber] = useState(1);
-
-    // function onDocumentLoadSuccess(numPages) {
-    //     setNumPages(numPages);
-    // }
     const plugins = defaultLayoutPlugin();
     return (
         <>
             <div className="w-full h-[900px] flex justify-center items-center overflow-y-auto">
                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                    <Viewer fileUrl={pdfFile} plugins={[plugins]} />
+                    <Viewer fileUrl={researchPaper} plugins={[plugins]} />
                 </Worker>
-            </div> 
+            </div>
         </>
     );
 };
